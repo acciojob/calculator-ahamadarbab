f3c23b12122eb908ef8b9dbe9d6176b33aa46a44
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-07-01 00:57:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your code here
+const input = document.getElementById("input");
+
+// Number Buttons
+for(let i = 0; i <= 9; i++) {
+	document
+		.getElementById(`block${i}`)
+		.addEventListener("click", () => {
+			input.value += i;
+		});
+}
+
+// Operators
+document.getElementById("plus").onclick = () => input.value += "+";
+document.getElementById("minus").onclick = () => input.value += "-";
+document.getElementById("multiply").onclick = () => input.value += "*";
+document.getElementById("divide").onclick = () => input.value += "/";
+document.getElementById("dot").onclick = () => input.value += ".";
+
+// Clear
+document.getElementById("clr").onclick = () => input.value = "";
+
+// Answer
+document.getElementById("ans").onclick = () => {
+	try {
+		input.value = eval(input.value);
+	} catch (e) {
+		input.value = "Error";
+	}
+};
